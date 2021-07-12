df3f44b4bdc43dc93a3def5fc0f426ac8f19ff6e
laluka <loukajc@gmail.com>
2021-07-12 12:54:42 +0000

    updated wordlist :)

--- make-wordlist.sh
+++ make-wordlist.sh
@@ -7,6 +7,8 @@ grep -viF 'Nxrs4tAtO' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst 
 grep -viF '/nmaplowercheck' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst pypotomux.lst
 grep -viF '/Nmap/folder' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst pypotomux.lst
 grep -viF '/NmapUpperCheck' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst pypotomux.lst
+grep -viF '/3000D00E0000' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst pypotomux.lst
+grep -viF '/204.48.20.180' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst pypotomux.lst
 
 # Sanitize
 grep -viF '/set_ftp.cgi?' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst pypotomux.lst
@@ -37,6 +39,9 @@ grep -viF '/include/makecvs.php?' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/py
 echo '/include/makecvs.php?' >> pypotomux.lst
 grep -viF '/cgi-bin/gw.cgi?' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst pypotomux.lst
 echo '/cgi-bin/gw.cgi?' >> pypotomux.lst
+grep -viF 'cgi-bin/nobody/Search.cgi?' pypotomux.lst > /tmp/pypotomux.lst; mv /tmp/pypotomux.lst pypotomux.lst
+echo 'cgi-bin/nobody/Search.cgi?' >> pypotomux.lst
+
 
 # Better formatting
 sed -i -e "s#^/*##g" pypotomux.lst
@@ -44,4 +49,4 @@ sort -uV pypotomux.lst -o pypotomux.lst
 
 echo "Sanitization done for known threats in pypotomux.lst BUT remember to check it MANUALLY as well"
 echo "Potential risky payloads?"
-cat pypotomux.lst | grep -viP "phpMyAdmin-[\d]+\.[\d]+\.[\d]+\.[\d]+" | grep --color=always -P "rm|cd|curl|wget|[\d]+\.[\d]+\.[\d]+\.[\d]+"
+cat pypotomux.lst | grep -viP "boaform/admin/form|GponForm/diag_Form|_cat/indices|phpMyAdmin-[\d]+\.[\d]+\.[\d]+\.[\d]+" | grep --color=always -P "rm|cd|curl|wget|[\d]+\.[\d]+\.[\d]+\.[\d]+"
