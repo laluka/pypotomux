c822225ec0f12c43b96b21162a5b31bf495c9ef0
TheLaluka <loukajc@gmail.com>
2021-09-04 16:45:29 +0200

    cleaned whole repo

--- make-wordlist-http-user-agent.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-
-find dump/http  -type f -name "*.json" -exec jq -r 'with_entries( .key |= ascii_downcase ) ."user-agent"' {} \; | sort -uV -o wordlists/user-agent.lst
-find dump/https -type f -name "*.json" -exec jq -r 'with_entries( .key |= ascii_downcase ) ."user-agent"' {} \; | sort -uV -o wordlists/user-agent.lst
-
--- make-wordlist-ssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in dump/ssh/*.json; do cat "$f" | jq -r '. | "\(.username):\(.password)"' ; done | sort -uV -o /tmp/new-pypotomux-ssh.lst
-cat /tmp/new-pypotomux-ssh.lst pypotomux-ssh.lst | sort -uV -o pypotomux-ssh.lst
-echo "Wordlist updated: pypotomux-ssh.lst"
-
